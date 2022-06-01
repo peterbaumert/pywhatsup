9865fd2290c93876e8a15dd787148c47c38c46ce
peterbaumert <peter.baumert@outlook.com>
2022-06-01 16:01:26 +0200

    lint fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/peterbaumert/pywhatsup",
-    packages=setuptools.find_packages(include=['pywhatsup']),
+    packages=setuptools.find_packages(include=["pywhatsup"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
